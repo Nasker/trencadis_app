a9dd0f8e89915124bb6854289aabf554f5432321
NaskerFo4 <xamps23@gmail.com>
2026-07-16 21:38:54 +0200

    Updated version and title font on the site

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.trencadis.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
