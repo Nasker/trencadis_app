95aa0f9d7a2e9872ca49655bd664c55b1b52894c
NaskerFo4 <xamps23@gmail.com>
2026-09-04 19:04:52 +0200

    V1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.trencadis.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.6"
+        versionCode = 5
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
